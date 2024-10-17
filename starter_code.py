6cbf794e0bb8d06d73edd30dc91da8787bfc31bd
topshenyi-web <erxu@ctemc.org>
2024-10-16 22:19:44 -0400

    final push

--- starter_code.py
+++ starter_code.py
@@ -8,6 +8,7 @@ import alignment
 from sklearn.metrics import r2_score
 from sklearn.ensemble import RandomForestRegressor
 
+import random
 
 # Your task is to *accurately* predict the primer melting points using machine 
 # learning based on the sequence of the primer.
@@ -30,12 +31,11 @@ def GetMeltingPoint(primer:str,melting_point_rf):
 def get_smallest_index_match_on_template_sequence(template_sequence:str, primer:str):
     scoring = alignment.ScoreParam(10,-5,-7)
     # binding if at least 80% max score
-    SCORE_THRESHOLD = .8 * (scoring.match*len(primer))
+    SCORE_THRESHOLD = .9 * (scoring.match*len(primer))
     best, optloc, matrix = alignment.local_align(template_sequence, primer, score=scoring)
     if best < SCORE_THRESHOLD:
         return 9999999
-
-    return optloc[0]-1 # the more straight-forward approach
+    return optloc[0]-1+1 # the more straight-forward approach
 
     for i in range(len(template_sequence)+1):
         for j in range(len(primer),-1,-1):
@@ -46,9 +46,12 @@ def get_smallest_index_match_on_template_sequence(template_sequence:str, primer:
             
 
 def reverse_complement(primer:str):
-    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C','N':'N'}
     comp_match = ''.join(complement[base] for base in reversed(primer))
     return comp_match
+
+def reverse(primer:str):
+    return primer[-1::-1]
 
 def complement(primer:str):
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
@@ -76,12 +79,15 @@ def PredictPCRProduct(primer1, primer2, template_sequence, melting_point_rf, ski
         return None
         
     #Fail any primers with conflicting melting points 
-    melting_point_1, melting_point_2 = GetMeltingPoint(primer1,melting_point_rf), GetMeltingPoint(primer2,melting_point_rf)
-    if not skip_temp and (abs(melting_point_1-60)>2 or abs(melting_point_2-60)>2):
-        # print("temps not right",melting_point_1,melting_point_2)
-        return None
-        
+    if not skip_temp:
+        melting_point_1, melting_point_2 = GetMeltingPoint(primer1,melting_point_rf), GetMeltingPoint(primer2,melting_point_rf)
+        if (abs(melting_point_1-60)>2 or abs(melting_point_2-60)>2):
+            # print("temps not right",melting_point_1,melting_point_2)
+            return None
+            
     #Check for binding on both primers
+    # top_strand_match = get_smallest_index_match_on_template_sequence(template_sequence = template_sequence, primer = reverse_complement(primer1))    
+    # bottom_strand_match = len(template_sequence)-1-get_smallest_index_match_on_template_sequence(template_sequence = reverse_complement(template_sequence), primer = reverse_complement(primer2))
     top_strand_match = min(
         get_smallest_index_match_on_template_sequence(template_sequence = template_sequence, primer = reverse_complement(primer1)),
         get_smallest_index_match_on_template_sequence(template_sequence = template_sequence, primer = reverse_complement(primer2)))
@@ -89,38 +95,89 @@ def PredictPCRProduct(primer1, primer2, template_sequence, melting_point_rf, ski
     bottom_strand_match = len(template_sequence)-1-(
         min(get_smallest_index_match_on_template_sequence(template_sequence = reverse_complement(template_sequence), primer = reverse_complement(primer1)),
             get_smallest_index_match_on_template_sequence(template_sequence = reverse_complement(template_sequence), primer = reverse_complement(primer2)))) # reverse the primers
-
-    if top_strand_match-bottom_strand_match<=0 or bottom_strand_match<0 or top_strand_match > 99999:
+    # print(top_strand_match,bottom_strand_match)
+    if top_strand_match>=bottom_strand_match or bottom_strand_match<0 or top_strand_match > 99999 or bottom_strand_match-top_strand_match>1000:
         return None # no overlap
     
     
-
-    return template_sequence[bottom_strand_match:top_strand_match+1]
+    # print(template_sequence[top_strand_match:bottom_strand_match+1])
+    return template_sequence[top_strand_match:bottom_strand_match+1]
 actual_seqs = ["tcccggatgttagcggcggacgggtgagtaacacgtgggtaacctgcctgtaagactgggataactccgggaaaccggagctaataccggatagttccttgaaccgcatggttcaaggatgaaagacggtttcggctgtcacttacagatggacccgcggcgcattagctagttggtgaggtaacggctcaccaaggcgacgatgcgtagccgacctgagagggtgatcggccacactgggactgagacacggcccagactcctacgggaggcagcagtagggaatcttccgcaatggacgaaagtctgacggagcaacgccgcgtgagtgatgaaggttttcggatcgtaaagctctgttgttagggaagaacaagtgcaagagtaactgcttgcaccttgacggtacctaaccagaaagccacggctaactacgtgccagcagccgcggtaatacgtaggtggcaagcgttgtccggAattattgggcgtAaagggctcgcaggcggtttcttaagtCtgatgtgaaagcccccggctcaaccggggagggtcattggaaactgggaaacttgagtgcagaagaggagagtggaattccacgtgtagcggtgaaatgcgtagagatgtggaggaacaccagtggcgaaggcgactctctggtctgtaactgacgctgaggagcgaaagcgtggggagcgaacaggattagataccctggtagtccacgccgtaaacgatgagtgctaagtgttagggggtttccgccccttagtgctgcagctaacgcattaagcactccgcctggggagtacggtcgcaagactgaaactcaaaggaattgacgggggcccgcacaagcggtggagcatgtggtttaattcgaagcaacgcgaagaaccttaccaggtcttgacatcctctgacaaccctagagatagggctttcccttcggggacagagtgacaggtggtgcatggttgtcgtcagctcgtgtcgtgagatgttgggttaagtcccgcaacgagcgcaacccttgatcttagttgccagcattcagttgggcactctaaggtgactgccggtgacaaaccggaggaaggtggggatgacgtcaaatcatcatgccccttatgacctgggctacacacgtgctacaatggacagaacaaagggctgcgagaccgcaaggtttagccaatcccacaaatctgttctcagttcggatcgcagtctgcaactcgactgcgtgaagctggaatcgctagtaatcgcggatcagcatgccgcggtgaatacgttcccgggccttgtacacaccgcccgtcacaccacgagagtttgcaacacccgaagtcggtgaggtaactttatggagccagcc","ggcgtgcctaatacatgcaagtcgagcggatcgatgggagcttgcnncntgagatcagcggcggacgggtgagtaacacgtgggtaacctgcctgtaagactgggataactccgggaaaccggggctaataccggataacacctacccccgcatgggggaaggttgaaaggtggcttcggctatcacttacagatggacccgcggcgcattagctagttggtgaggtaatggctcaccaaggcgacgatgcgtagccgacctgagagggtgatcggccacactgggactgagacacggcccagactcctacgggaggcagcagtagggaatcttccgcaatggacgaaagtctgacggagcaacgccgcgtgagtgaagaaggttttcggatcgtaaaactctgttgttagggaagaacaagtgccgttcgaatagggcggcgccttgacggtacctaaccagaaagccacggctaactacgtgccagcagccgcggtaatacgtaggtggcaagcgttgtccggaattattgggcgtaaagcgcgcgcaggtggtttcttaagtctgatgtgaaagcccacggctcaaccgtggagggtcattggaaactggggaacttgagtgcagaagaggaaagtggaattccaagtgtagcggtgaaatgcgtagatatttggaggaacaccagtggcgaaggcgactttctggtctgtaactgacactgaggcgcgaaagcgtggggagcaaacaggattagataccctggtagtccacgccgtaaacgatgagtgctaagtgttagagggtttccgccctttagtgctgcagctaacgcattaagcactccgcctggggagtacggtcgcaagactgaaactcaaaggaattgacgggggcccgcacaagcggtggagcatgtggtttaattcgaagcaacgcgaagaaccttaccaggtcttgacatcctctgacaaccctagagatagggctttccccttcgggggacagagtgacaggtggtgcatggttgtcgtcagctcgtgtcgtgagatgttgggttaagtcccgcaacgagcgcaacccttgatcttagttgccagcattcagttgggcactctaagatgactgccggtgacaaaccggaggaaggtggggatgacgtcaaatcatcatgccccttatgacctgggctacacacgtgctacaatggacggtacaaagggctgcaagaccgcgaggtttagccaatcccataaaaccgttctcagttcggattgtaggctgcaactcgcctacatgaagctggaatcgctagtaatcgcgnatcagcatgccgcggtgaatacgttcccgggccttgtacacaccgcccgtcacaccacgagagtttgtaaca"]
 actual_seqs = [seq.upper() for seq in actual_seqs]
+
+
 def task_3(sequences,rf):
     def test_primer_validity(forward_primer,backward_primer):
+        product_lens = []
         for sequence in sequences:
-            product = PredictPCRProduct(forward_primer,backward_primer,sequence,rf,skip_temp=True)
+            product = PredictPCRProduct(forward_primer,backward_primer,sequence,rf,skip_temp=False)
+            # print(product)
             if product is None:
                 return False
-        for sequence in sequences:
-            product = PredictPCRProduct(forward_primer,backward_primer,sequence,rf)
-            if product is None:
-                return False
-        return True
+            product_lens.append(len(product))
+        # for sequence in sequences:
+        #     product = PredictPCRProduct(forward_primer,backward_primer,sequence,rf)
+        #     if product is None:
+        #         return False
+        return abs(product_lens[0]-product_lens[1])>=300
     ref_seq = sequences[0]
-    for primer_length in range(30,17,-1):
-        for i in range(0,len(ref_seq)-primer_length):
-            for j in range(0,len(ref_seq)-1-(i+primer_length-1)-primer_length):
-                fp = ref_seq[i:primer_length+i]
-                bp = reverse_complement(ref_seq)[j:primer_length+j]
+    for primer_length in range(23,17,-1):
+        while True:
+            i = random.randint(0,len(ref_seq)-primer_length-1)
+            j = random.randint(0,max(0,len(ref_seq)-1-(i+primer_length-1)-primer_length-1))
 
-                fp = reverse_complement(fp)
-                bp = reverse_complement(bp)
+            # for i in range(0,len(ref_seq)-primer_length):
+            #     for j in range(0,len(ref_seq)-1-(i+primer_length-1)-primer_length):
+            fp = ref_seq[i:primer_length+i]
+            bp = reverse_complement(ref_seq)[j:primer_length+j]
 
-                if(test_primer_validity(fp,bp)):
-                    print(fp,bp)
+            fp = reverse_complement(fp)
+            bp = reverse_complement(bp)
+            assert len(fp)==len(bp)==primer_length
+            if(test_primer_validity(fp,bp)):
+                print(fp,bp,i,j)
+
+
+def task_5(sequences,rf):
+    def test_primer_validity(forward_primer,backward_primer):
+        product_lens = []
+        for sequence in sequences:
+            product = PredictPCRProduct(forward_primer,backward_primer,sequence,rf,skip_temp=False)
+            # print(product)
+            if product is None:
+                return False
+            product_lens.append(len(product))
+        # for sequence in sequences:
+        #     product = PredictPCRProduct(forward_primer,backward_primer,sequence,rf)
+        #     if product is None:
+        #         return False
+        return abs(product_lens[0]-product_lens[1])>=300
+    ref_seq = sequences[0]
+    for primer_length in range(23,17,-1):
+        while True:
+            i = random.randint(0,len(ref_seq)-primer_length-1)
+            j = random.randint(0,max(0,len(ref_seq)-1-(i+primer_length-1)-primer_length-1))
+            k = random.randint(0,max(0,len(sequences[1])-1-(i+primer_length-1)-primer_length-1))
+            # for i in range(0,len(ref_seq)-primer_length):
+            #     for j in range(0,len(ref_seq)-1-(i+primer_length-1)-primer_length):
+            fp = ref_seq[i:primer_length+i]
+            bp1 = reverse_complement(ref_seq)[j:primer_length+j]
+            bp2 = reverse_complement(sequences[1])[k:primer_length+k]
+
+            fp = reverse_complement(fp)
+            bp1 = reverse_complement(bp1)
+            bp2 = reverse_complement(bp2)
+            
+            assert len(fp)==len(bp1)==len(bp2)==primer_length
+            product_1 = PredictPCRProduct(fp,bp1,sequences[0],rf,skip_temp=False)
+            not_product_1 = PredictPCRProduct(fp,bp2,sequences[0],rf,skip_temp=False) or PredictPCRProduct(bp1,bp2,sequences[0],rf,skip_temp=False)
+            product_2 = PredictPCRProduct(fp,bp2,sequences[1],rf,skip_temp=False)
+            not_product_2 = PredictPCRProduct(fp,bp1,sequences[1],rf,skip_temp=False) or PredictPCRProduct(bp1,bp2,sequences[1],rf,skip_temp=False)
+            # print(product_1,not_product_1,product_2,not_product_2)
+            if product_1 is not None and product_2 is not None and not not_product_1 and not not_product_2 and abs(len(product_1)-len(product_2))>=100:
+                print(abs(len(product_1)-len(product_2)))
+                print(product_1,product_2,fp,bp1,bp2)
+                
 def LoadFastA(path):
     infile = open(path, 'r')
     seq = ""
@@ -206,9 +263,11 @@ if __name__ == "__main__":
 
     #    print(PredictPCRProduct("ACTG", "ACTG", "ACTCAGCGACTGC", task2_randomforest))
     #    print(PredictPCRProduct("TGGTGGGATGTCTTTCAACAGG", "AACTACGGAGAACTACAGCAACCT","ACGTCAGCGAGCGCTACGACGTGGTGGGATGTCTTTCAACAGGACGGACTGACGCGACGACTGACTGTAGGCTAGGTTGCTGTAGTTCTCCGTAGTTAGCTACGACGCATGCAGCTGCA", task2_randomforest))
-    assert PredictPCRProduct("TGGTGGGATGTCTTTCAACAGG", "AACTACGGAGAACTACAGCAACCT","ACGTCAGCGAGCGCTACGACGTGGTGGGATGTCTTTCAACAGGACGGACTGACGCGACGACTGACTGTAGGCTAGGTTGCTGTAGTTCTCCGTAGTTAGCTACGACGCATGCAGCTGCA", task2_randomforest) == "TGGTGGGATGTCTTTCAACAGGACGGACTGACGCGACGACTGACTGTAGGCTAGGTTGCTGTAGTTCTCCGTAGTT"
+    assert PredictPCRProduct(reverse_complement("TGGTGGGATGTCTTTCAACAGG"), reverse_complement("AACTACGGAGAACTACAGCAACCT"),"ACGTCAGCGAGCGCTACGACGTGGTGGGATGTCTTTCAACAGGACGGACTGACGCGACGACTGACTGTAGGCTAGGTTGCTGTAGTTCTCCGTAGTTAGCTACGACGCATGCAGCTGCA", task2_randomforest) == "ACGGACTGACGCGACGACTGACTGTAGGCT"
+    # print(PredictPCRProduct("AAAGCCCTATCTCTAGGGTTGTC","ATGTTGGGTTAAGTCCCGCAACG",actual_seqs[0],task2_randomforest))
+    # print(PredictPCRProduct("AAAGCCCTATCTCTAGGGTTGTC","ATGTTGGGTTAAGTCCCGCAACG",actual_seqs[1],task2_randomforest))
 
-    task_3(actual_seqs,task2_randomforest)
+    task_5(actual_seqs,task2_randomforest)
     """
     Task 3:
     Design primers for a PCR reaction to distinguish between the three (strands)
